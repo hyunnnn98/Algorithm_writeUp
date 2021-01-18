a366d5e747f39db2a9b0e59a64a7e4ab2bf2b077
SeungHyun Cho <c0305sh@naver.com>
2021-01-18 23:09:37 +0900

    배열 원소 중 양의 정수의 최솟값의 다음 수 찾기

---
+++ 알고리즘__016_최소 정수 찾기.js
@@ -0,0 +1,40 @@
+
+/**
+ * 
+ * function solution(A);
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+ */
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    // sorting
+
+    A.sort(function (a, b) {
+        return a - b;
+    });
+    let answer = 0
+
+    for (let i = 0; i < A.length; i++) {
+        if (answer < 0 && answer < A[i]) {
+            answer = A[i]
+        } else if (answer >= 0 && answer < A[i] && answer + 1 == A[i]) {
+            answer = A[i]
+        }
+    }
+
+    return answer + 1
+}
+
+console.log(solution([-1, -3]))
